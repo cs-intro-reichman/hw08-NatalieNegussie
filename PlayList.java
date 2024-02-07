3c681b533b955fc47a55624e073b9bf993f4b543
Your Name <your.name@runi.ac.il>
2024-02-07 23:02:40 +0200

    hi

--- PlayList.java
+++ PlayList.java
@@ -24,7 +24,7 @@ class PlayList {
 
     /** Method to get a track by index */
     public Track getTrack(int index) {
-        if (index >= 0 && index < size) {
+        if(index >= 0 && index < size) {
             return tracks[index];
         } else {
             return null;
@@ -35,32 +35,48 @@ class PlayList {
      *  If the list is full, does nothing and returns false.
      *  Otherwise, appends the track and returns true. */
     public boolean add(Track track) {
-        //// replace the following statement with your code
-        return true;
+        if(size < maxSize) {
+            tracks[size++] = track;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /** Returns the data of this list, as a string. Each track appears in a separate line. */
     //// For an efficient implementation, use StringBuilder.
     public String toString() {
-        //// replace the following statement with your code
-        return "";
+        StringBuilder listData = new StringBuilder();
+        for(int i = 0; i < size; i++) {
+            listData.append(tracks[i].toString()).append("\n");
+        }
+        return listData.toString();
     }
 
     /** Removes the last track from this list. If the list is empty, does nothing. */
      public void removeLast() {
-        //// replace this comment with your code
+        if(size > 0) {
+            size--;
+        }
     }
     
     /** Returns the total duration (in seconds) of all the tracks in this list.*/
     public int totalDuration() {
-        //// replace the following statement with your code
-        return 0;
+        int total= 0;
+        for(int i = 0; i < size; i++){
+            total+= tracks[i].getDuration();
+        }
+        return total;
     }
 
     /** Returns the index of the track with the given title in this list.
      *  If such a track is not found, returns -1. */
-    public int indexOf(String title) {
-        //// replace the following statement with your code
+    public int indexOf(String title){
+        for(int i = 0; i< size; i++){
+            if(tracks[i].getTitle().equalsIgnoreCase(title)){
+                return i;
+            }
+        }
         return -1;
     }
 
